2aa0e0f2e248ba12cba76fbc5708fab3bd7ba009
wuyulei-dev <1609091613@qq.com>
2023-03-22 10:38:43 +0800

    测试修改项目名

---
+++ src/main/java/com/secrity/sdfsdf.java
@@ -0,0 +1,19 @@
+/*
+ * @(#)sdfsdf.java
+ * Copyright (C) 2020 Neusoft Corporation All rights reserved.
+ *
+ * VERSION        DATE       BY              CHANGE/COMMENT
+ * ----------------------------------------------------------------------------
+ * @version 1.00  2023年3月22日 wwp-pc          初版
+ *
+ */
+package com.secrity;
+
+public class sdfsdf {
+
+    public static void main(
+        String[] args) {
+
+    }
+
+}
